eb0e7600291c6ae75fa73a3f7a5b31eaf3164757
Mo <moritz.schubert@sopat.de>
2023-11-21 13:06:24 +0100

    first simple calculator version ready

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -36,10 +36,11 @@ namespace Project1 {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label2;
 	protected:
 
 	private:
@@ -57,10 +58,10 @@ namespace Project1 {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -73,7 +74,7 @@ namespace Project1 {
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"x2";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::my_name_Click);
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// textBox1
 			// 
@@ -83,22 +84,14 @@ namespace Project1 {
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(106, 49);
 			this->textBox1->TabIndex = 1;
-			// 
-			// textBox2
-			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(306, 75);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(106, 49);
-			this->textBox2->TabIndex = 2;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Location = System::Drawing::Point(180, 9);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(422, 42);
 			this->label1->TabIndex = 3;
@@ -114,27 +107,40 @@ namespace Project1 {
 			this->button2->TabIndex = 4;
 			this->button2->Text = L"x3";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// button3
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(166, 296);
+			this->button3->Location = System::Drawing::Point(19, 292);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(127, 44);
+			this->button3->Size = System::Drawing::Size(407, 56);
 			this->button3->TabIndex = 5;
-			this->button3->Text = L"-2";
+			this->button3->Text = L"Wie ist mein Name\?";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(516, 355);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(174, 42);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Ausgabe";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(539, 422);
+			this->ClientSize = System::Drawing::Size(787, 422);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
@@ -145,7 +151,21 @@ namespace Project1 {
 		}
 #pragma endregion
 
-	private: System::Void my_name_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text);
+		label2->Text = System::Convert::ToString(output);
 	}
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text)*2;
+		label2->Text = System::Convert::ToString(output);
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		int output = System::Convert::ToInt16(textBox1->Text) * 3;
+		label2->Text = System::Convert::ToString(output);
+	}
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+		label2->Text = System::Convert::ToString("Moritz");
+	}
+};
 }
